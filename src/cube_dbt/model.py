7d99d4a00bfa70fd08209b8e58ad7294fe3d82d1
samuelberntzen <samuel.berntzen@protonmail.com>
2024-03-26 14:27:56 +0100

    testing generation of measures from dbt meta in manifest.json

--- src/cube_dbt/model.py
+++ src/cube_dbt/model.py
@@ -1,11 +1,14 @@
 from cube_dbt.column import Column
 from cube_dbt.dump import SafeString, dump
+from cube_dbt.measure import Measure
+from cube_dbt.test import Test
 
 
 class Model:
     def __init__(self, model_dict: dict) -> None:
         self._model_dict = model_dict
         self._columns = None
+        self._measures = None
         self._primary_key = None
         self._tests = []
         pass
@@ -21,6 +24,16 @@ class Model:
             )
             self._detect_primary_key()
 
+    def _init_measures(self) -> None:
+        if self._measures == None:
+            measures = self._model_dict["meta"].get("measures", {})
+            if measures:
+                self._measures = list(
+                    Measure(measure) for measure in self._model_dict["meta"]["measures"]
+                )
+            else:
+                self._measures = []
+
     def _detect_primary_key(self) -> None:
         candidates = list(column for column in self._columns if column.primary_key)
 
@@ -31,6 +44,9 @@ class Model:
             )
 
         self._primary_key = candidates[0] if len(candidates) == 1 else None
+
+    def add_test(self, test: "Test") -> None:
+        self._tests.append(test)
 
     @property
     def name(self) -> str:
@@ -59,14 +75,31 @@ class Model:
         self._init_columns()
         return self._columns
 
-    def column(self, name: str) -> Column:
-        self._init_columns()
-        return next(column for column in self._columns if column.name == name)
+    @property
+    def tests(self) -> list:
+        return self._tests
 
     @property
     def primary_key(self) -> Column or None:
         self._init_columns()
         return self._primary_key
+
+    @property
+    def measures(self) -> list[Measure]:
+        self._init_measures()
+        return self._measures
+
+    def column(self, name: str) -> Column:
+        self._init_columns()
+        return next(column for column in self._columns if column.name == name)
+
+    def measure(self, name: str) -> Measure:
+        self._init_measures()
+        return next(measure for measure in self._measures if measure.name == name)
+
+    def test(self, name: str) -> Test:
+        self._init_tests()
+        return next(test for test in self._tests if test.name == name)
 
     def _as_cube(self) -> dict:
         data = {}
@@ -96,13 +129,6 @@ class Model:
         dimensions = self._as_dimensions(skip)
         return dump(dimensions, indent=6) if dimensions else SafeString("")
 
-    def add_test(self, test) -> None:
-        self._tests.append(test)
-
-    @property
-    def tests(self) -> list:
-        return self._tests
-
     def _as_joins(self) -> list:
         return list(test._as_join() for test in self.tests)
 
@@ -113,3 +139,14 @@ class Model:
         """
         joins = self._as_joins()
         return dump(joins, indent=6) if joins else SafeString("")
+
+    def _as_measures(self) -> list:
+        return list(measure._as_measure() for measure in self.measures)
+
+    def as_measures(self) -> str:
+        """
+        For use in Jinja:
+        {{ dbt.model('name').as_measures(skip=['id']) }}
+        """
+        measures = self._as_measures()
+        return dump(measures, indent=6) if measures else SafeString("")
